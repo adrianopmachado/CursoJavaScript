13b9808738ceb232bb8de3f13dd9407803da33be
Adriano Parente <adrianopmachado@outlook.com>
2022-10-27 19:26:52 -0300

    atualização

---
+++ Básico/7 - Tipos primitivos/index.js
@@ -0,0 +1,11 @@
+// string, number, undefined, null, boolean, symbol
+const nome = 'Adriano'; // String
+const num = 7 ;// number
+const num2 = 10.8; // number
+let nomeAluno; // undefined = Não aponta pra local nenhum na memória
+let sobrenomeAluno = null; // Nulo = Não aponta pra local nenhum na memória
+const verdadeiro = true; // Boolean = Verdadeiro ou falso (true ou false)
+const falso = false; // Boolean = Verdadeiro ou falso (true ou false) = (lógico)
+
+console.log(typeof nome);
+
